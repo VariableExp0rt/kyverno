5d52e092079b6a64f8239cc18eec1e193f42b274
liam.baker <liam.baker@sage.com>
2023-06-15 17:30:00 +0100

    remove unnecessary ordering
    Signed-off-by: liam.baker <liam.baker@sage.com>

--- pkg/engine/context/evaluate.go
+++ pkg/engine/context/evaluate.go
@@ -53,10 +53,10 @@ func (ctx *context) getMatchingLoaders(query string) []DeferredLoader {
 	ctx.deferred.mutex.Lock()
 	defer ctx.deferred.mutex.Unlock()
 	var matchingLoaders []DeferredLoader
-	for _, name := range ctx.deferredOrder {
+	for name, deferredLoader := range ctx.deferred.loaders {
 		if strings.Contains(query, name) {
 			matchingLoaders = append(matchingLoaders, func() error {
-				if deferredLoader, ok := ctx.deferred.loaders[name]; ok {
+				if _, ok := ctx.deferred.loaders[name]; ok {
 					delete(ctx.deferred.loaders, name)
 					return deferredLoader()
 				}
